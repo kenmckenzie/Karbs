8241a5f043901ef360f0a4b48e2cbe7cd811abc1
Ken McKenzie <slain.12@gmail.com>
2020-08-14 19:50:37 +0200

    Changed apps plus minor script change

--- larbs.sh
+++ larbs.sh
@@ -151,7 +151,7 @@ systembeepoff() { dialog --infobox "Getting rid of that retarded error beep soun
 
 finalize(){ \
 	dialog --infobox "Preparing welcome message..." 4 50
-	dialog --title "All done!" --msgbox "Congrats! Provided there were no hidden errors, the script completed successfully and all the programs and configuration files should be in place.\\n\\nTo run the new graphical environment, log out and log back in as your new user, then run the command \"startx\" to start the graphical environment (it will start automatically in tty1).\\n\\n.t Luke" 12 80
+	dialog --title "All done!" --msgbox "Congrats! Provided there were no hidden errors, the script completed successfully and all the programs and configuration files should be in place.\\n\\nTo run the new graphical environment, enable lightdm.service" 12 80
 	}
 
 ### THE ACTUAL SCRIPT ###
